10557034205263e4fc2fac4627122bf781a1dc90
cravsky <cravsky@gmail.com>
2024-04-23 11:45:57 +0200

    ref vs state vs variable

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,14 +1,43 @@
+import { useState, useRef } from "react";
+
 export default function TimerChallenge({ title, targetTime }) {
+    const timer = useRef(); // timer is component insant specific
+    // ref vs state vs variable
+    
+    // ref: for component instance specific data, that doesn't trigger re-render.
+    // refs should not display on the UI
+    // state: for component instance specific data, that triggers re-render
+    // variable: gets re-created on every re-render
+
+    const [timerStarted, setTimerStarted] = useState(false);
+    const [timerExpired, setTimerExpired] = useState(false);
+
+
+    function handleStart() {
+        timer.current = setTimeout(() => {
+            setTimerExpired(true);
+        }, targetTime * 1000);
+
+        setTimerStarted(true);
+    }
+
+    function handleStop() {
+        clearTimeout(timer.current);
+    }
+
     return <section className="challenge">
         <h2>{title}</h2>
+        {timerExpired && <p>You lost!</p>}
         <p className="challenge-time">
             {targetTime} second{targetTime > 1 ? 's' : ''}
         </p>
         <p>
-            <button>
-                Start Challange
+            <button onClick={timerStarted ? handleStop : handleStart}>
+                {timerStarted ? 'Stop' : 'Start'} Challange
             </button>
         </p>
-        <p className="">Time is running.../ Timer inactive</p>
+        <p className={timerStarted ? 'active' : undefined}>
+            {timerStarted ? 'Time is running...' : 'Timer inactive'}
+        </p>
     </section>
 }
